157865926678736078b608e14671f565837225a5
Jack Hall <jackvhall@outlook.com>
2019-01-19 08:44:25 -0800

    all week 8 deliverables

---
+++ Week-8 Deliverables/OffensivePlayer.java
@@ -0,0 +1,114 @@
+/**
+ * @author Jack Hall
+ * @course CST-105
+ * @professor Amr Elchouemi
+ *            <p>
+ *            This code was written by me for class - week 7.
+ * @since 01-10-2019
+ */
+public class OffensivePlayer extends Player {
+	private int passingCompletions;
+	private int passingAttempts;
+	private int touchdowns;
+	private int interceptions;
+	private int yards;
+
+	/**
+	 * Full argument constructor
+	 * 
+	 * @param name
+	 * @param college
+	 * @param position
+	 * @param year
+	 * @param jersey
+	 * @param weight
+	 * @param height
+	 * @param age
+	 * @param seasons
+	 * @param passingCompletions
+	 * @param passingAttempts
+	 * @param touchdowns
+	 * @param interceptions
+	 * @param yards
+	 */
+	public OffensivePlayer(String name, String college, String position, String year, int jersey, int weight,
+			int height, int age, int seasons, int passingCompletions, int passingAttempts, int touchdowns,
+			int interceptions, int yards) {
+		super(name, college, position, year, jersey, weight, height, age, seasons);
+		// TODO Auto-generated constructor stub
+		this.passingCompletions = passingCompletions;
+		this.passingAttempts = passingAttempts;
+		this.touchdowns = touchdowns;
+		this.interceptions = interceptions;
+		this.yards = yards;
+
+	}
+
+	/**
+	 * No argument constructor
+	 */
+	public OffensivePlayer() {
+		this(null, null, null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	}
+
+	// setters
+	public void setPassingCompletions(int completions) {
+		this.passingCompletions = completions;
+	}
+
+	public void setPassingAttempts(int attempts) {
+		this.passingAttempts = attempts;
+	}
+
+	public void setTouchdowns(int touchdowns) {
+		this.touchdowns = touchdowns;
+	}
+
+	public void setInterceptions(int interceptions) {
+		this.interceptions = interceptions;
+	}
+
+	public void setYards(int yards) {
+		this.yards = yards;
+	}
+
+	// getters
+	public int getPassingCompletions() {
+		return passingCompletions;
+	}
+
+	public int getPassingAttempts() {
+		return passingAttempts;
+	}
+
+	public int getTouchdowns() {
+		return touchdowns;
+	}
+
+	public int getInterceptions() {
+		return interceptions;
+	}
+
+	public int getYards() {
+		return yards;
+	}
+	/**
+	 * Calculates percentage of passing completions
+	 * @return result
+	 */
+	public double getPercentageCompletions() {
+		double result = (1.0 * passingCompletions) / passingAttempts * 100.0;
+		return result;
+	}
+	
+	/**
+	 *  Overidden toString method that includes super.toString()
+	 */
+	@Override
+	public String toString() {
+		return super.toString() + "\nas OffensivePlayer [passingCompletions=" + passingCompletions
+				+ ", passingAttempts=" + passingAttempts + ", touchdowns=" + touchdowns + ", interceptions="
+				+ interceptions + ", yards=" + yards + "]";
+	}
+
+}
---
+++ Week-8 Deliverables/PalindromeAndPrime.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ * @author Jack Hall
+ * @professor Amr Elchouemi
+ * @course CST-105
+ * @since 1-15-19 I completed this work for CST-105 class.
+ */
+public class PalindromeAndPrime {
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count = 0;
+		for (int i = 0; count < 100; i++) {
+
+			if (isPrime(i) && isPalindrome(i)) {
+				System.out.printf("%10d", i);
+				count++;
+				if (count % 10 == 0 && i != 0)
+					System.out.println();
+
+			}
+		}
+
+	}
+
+	/**
+	 * Method to determine if a number is prime. Divides each number by each number
+	 * before it, stopping when i <= n/2. If a divisor producing a remainder of 0 is
+	 * found, returns false. Otherwise, if no divisor produces a remainder of 0,
+	 * number is prime and method returns true.
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public static boolean isPrime(int n) {
+
+		// ignore numbers before the first prime number
+		if (n < 2)
+			return false;
+
+		for (int i = 2; i <= n / 2; i++) {
+
+			if (n % i == 0)
+				return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to determine if a number is palindrome. Calls reverse method to
+	 * determined if number == to the reverse of number.
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static boolean isPalindrome(int number) {
+		return (number == reverse(number));
+	}
+
+	/**
+	 * Method to reverse the number digit by digit. Iterates through numbers and
+	 * places the final digit of number as the first digit of reversed, and so on
+	 * and so forth.
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static int reverse(int number) {
+		int reversed = 0;
+		while (number > 0) {
+			reversed = reversed * 10;
+			reversed += number % 10;
+			number = number / 10;
+		}
+		return reversed;
+	}
+
+}
